c6bef126e9e78546ad3022afcec2995da372f0c5
AkeelMNM <akeel1447@gmail.com>
2020-08-14 15:22:06 +0530

    initial commit

---
+++ app/src/main/java/com/firstapp/jobfixer/AdminHomeActivity.java
@@ -0,0 +1,22 @@
+package com.firstapp.jobfixer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class AdminHomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_home);
+    }
+
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
