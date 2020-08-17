231b2c981d919c61f46c90b5385206e0d1dfa138
nushkymohamed <nuskymohamed1999@gmail.com>
2020-08-17 21:53:09 +0530

    my function interfaces are complete

---
+++ app/src/main/java/com/firstapp/jobfixer/AdminRequestsActivity.java
@@ -0,0 +1,21 @@
+package com.firstapp.jobfixer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class AdminRequestsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_requests);
+    }
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+        return true;
+    }
+}
