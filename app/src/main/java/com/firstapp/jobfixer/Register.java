875b731801b03bd0c8384681f427e1c15ff5ed94
Asram Ahamed <youngunner1998@gmail.com>
2020-08-17 20:25:05 +0530

    My functions interface designing was completed

---
+++ app/src/main/java/com/firstapp/jobfixer/Register.java
@@ -0,0 +1,18 @@
+package com.firstapp.jobfixer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register3);
+    }
+
+
+}
